88a48ae46aa21559e99b5504f708f2d6ffeb3c88
Hussein Al Rekabei <husseinalrekabei21@gmail.com>
2019-11-20 23:27:18 +0300

    add auth

---
+++ Iraqi.Heros/Controllers/AdminController.cs
@@ -0,0 +1,191 @@
+﻿using Iraqi.Heros.DAL;
+using Iraqi.Heros.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iraqi.Heros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles ="admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly MainDbContext _context;
+
+        public AdminController(MainDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Admin
+        [Authorize(Roles = ("admin"))]
+        [HttpGet("GetUsers")]
+      
+        public async Task<ActionResult<IEnumerable<Users>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/Admin/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUsers(Guid id)
+        {
+            var users = await _context.Users.FindAsync(id);
+
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return users;
+        }
+
+        // PUT: api/Admin/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("Update/{id}")]
+        
+        public async Task<IActionResult> PutUsers(Guid id, Users users)
+        {
+            users.Role = "admin";
+            if (id != users.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(users).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsersExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Admin
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost("addUser")]
+        public async Task<ActionResult<Users>> PostUsers(Users users)
+        {
+            users.Role = "admin";
+            _context.Users.Add(users);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsers", new { id = users.Id }, users);
+        }
+
+        // DELETE: api/Admin/5
+     
+
+        private bool UsersExists(Guid id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+
+
+
+        [HttpGet("All/{start}/{end}")]
+      
+        public async Task<IActionResult> All(int start, int end)
+        {
+            return Ok(
+                await _context.Persons.Include(x => x.Images).Where(x=>x.Status==0).Select(x => new
+                {
+                    x.Id,
+                    x.Name,
+                    x.PoK,
+                    x.Gov,
+                    x.DoB,
+                    x.Type,
+                    ImageName = x.Images.Select(z => new string($"{z.Name}{z.Key}")).First()
+                }
+
+                    ).AsNoTracking().OrderBy(x => x.Id).Skip(start).Take(end).ToListAsync()
+                ); 
+        }
+
+
+        [Authorize(Roles = ("admin"))]
+
+        [HttpPut("{id}/{status}")]
+        public async Task<IActionResult> UpdateStatus(Guid id, int status)
+        {
+            var result = await _context.Persons.FirstAsync(x => x.Id == id);
+            if (result == null)
+                return BadRequest(new {
+                Error="Not Found Person"
+                });
+            result.Status = status;
+            _context.Persons.Update(result);
+            await _context.SaveChangesAsync();
+            return Ok(result);
+        }
+
+
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginForm login)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x=>x.UserName==login.Username && x.Password==login.Password);
+            if (user == null)
+                return BadRequest(new
+                {
+                    Error = "UserName or Password Incorrect"
+                });
+       
+
+           
+
+            var claims = new[]
+            {
+                //  new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                new Claim("Username", user.UserName),
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim("Role", user.Role),
+                new Claim("id", user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+            var token = new JwtSecurityToken
+            (
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(3),
+                notBefore: DateTime.UtcNow,
+                audience: "Audience",
+                issuer: "Issuer",
+                signingCredentials: new SigningCredentials(
+                    new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes("Hlkjds0-324mf34pojf-34r34fwlknef0943")),
+                    SecurityAlgorithms.HmacSha256)
+            );
+            return Ok(new 
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+            });
+        }
+
+
+    }
+}
--- Iraqi.Heros/Startup.cs
+++ Iraqi.Heros/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Iraqi.Heros.DAL;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Iraqi.Heros
@@ -27,17 +30,41 @@ namespace Iraqi.Heros
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-                //services.AddDbContext<MainDbContext>(options =>
-                //    options.UseNpgsql(Configuration.GetConnectionString("Conaction")));
-
-                services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+             jwtBearerOptions =>
+             {
+                 jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateActor = true,
+                     ValidateAudience = true,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+                     ValidIssuer = "Issuer",
+                     ValidAudience = "Audience",
+                     IssuerSigningKey =
+                         new SymmetricSecurityKey(
+                             Encoding.UTF8.GetBytes("Hlkjds0-324mf34pojf-34r34fwlknef0943"))
+                 };
+             });
+            services.AddMvc(c => c.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddNewtonsoftJson(opt =>opt.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore); ;
+            services.AddDbContext<MainDbContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("Conaction")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
-        
-    }
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+            services.AddControllers();
+          
+         
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,27 +73,18 @@ namespace Iraqi.Heros
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors("CorsPolicy");
+            app.UseHttpsRedirection();
             app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
+            
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-
-
+            
             app.UseStaticFiles();
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-           
-
             app.UseAuthentication();
-
-       
+            app.UseMvc();
         }
     }
 }
